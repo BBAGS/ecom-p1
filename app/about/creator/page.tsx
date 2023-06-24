fedadc269c33c82866f51fe69764a3a059038ff3
ThomasA <thomasaditya228@gmail.com>
2023-06-25 00:54:45 +0900

    first commit

---
+++ app/about/creator/page.tsx
@@ -0,0 +1,9 @@
+export default function Creator() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <p>Thomas</p>
+      </div>
+    </main>
+  );
+}
