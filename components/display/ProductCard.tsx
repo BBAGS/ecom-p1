862f9b2c6f635400454f6b89f1691b07b1ae22c0
ThomasA <thomasaditya228@gmail.com>
2023-07-11 01:27:23 +0900

    added image to product (dummy)

--- components/display/ProductCard.tsx
+++ components/display/ProductCard.tsx
@@ -1,32 +1,43 @@
-import { motion } from 'framer-motion'
+import Image from "next/image";
+import { motion } from "framer-motion";
 
 type Product = {
   title: string;
   description: string;
   image: string;
   price: string;
-}
+};
 
-export type ProductCardProps = {
+type ProductCardProps = {
   product: Product;
-}
+};
 
 export default function ProductCard({ product }: ProductCardProps) {
   return (
     <div className="w-1/4 p-4">
-      <motion.div className="rounded overflow-hidden shadow-lg"
+      <motion.div
+        className="rounded overflow-hidden shadow-lg"
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95 }}
       >
-        <img className="w-full" src={product.image} alt={product.title} />
+        <div className="relative h-0" style={{ paddingBottom: "75%" }}>
+          <Image
+            fill
+            src={product.image}
+            alt={product.title}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">{product.title}</div>
           <p className="text-gray-700 text-base">{product.description}</p>
         </div>
         <div className="px-6 pt-4 pb-2">
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">${product.price}</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            ${product.price}
+          </span>
         </div>
       </motion.div>
     </div>
-  )
+  );
 }
--- components/sections/Catalase.tsx
+++ components/sections/Catalase.tsx
@@ -1,39 +1,55 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
-import CategoryTabs from '../navigation/CategoryTabs';
-import ProductCard from '../display/ProductCard';
-import { SectionWrapper } from '@/hoc';
+import { useState } from "react";
+import CategoryTabs from "../navigation/CategoryTabs";
+import ProductCard from "../display/ProductCard";
+import { SectionWrapper } from "@/hoc";
 
 type Product = {
   title: string;
   description: string;
   image: string;
   price: string;
-}
+};
 
 // function Catalase() {
 const Catalase = () => {
-  const [selectedCategory, setSelectedCategory] = useState('All');
+  const [selectedCategory, setSelectedCategory] = useState("All");
 
   const products: Product[] = [
-    { title: 'Product 1', description: 'Description 1', image: '/path/to/image1.jpg', price: '10.00' },
-    { title: 'Product 2', description: 'Description 2', image: '/path/to/image2.jpg', price: '20.00' },
+    {
+      title: "Product 1",
+      description: "Description 1",
+      image:
+        "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80",
+      price: "10.00",
+    },
+    {
+      title: "Product 2",
+      description: "Description 2",
+      image:
+        "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80",
+      price: "20.00",
+    },
     // More products...
   ];
 
-  const categories = ['All', 'Category 1', 'Category 2', 'Category 3'] // Replace with your actual categories
+  const categories = ["All", "Category 1", "Category 2", "Category 3"]; // Replace with your actual categories
 
   return (
     <section className="flex max-w-7xl m-auto">
-      <CategoryTabs categories={categories} selectedCategory={selectedCategory} onCategorySelect={setSelectedCategory} />
+      <CategoryTabs
+        categories={categories}
+        selectedCategory={selectedCategory}
+        onCategorySelect={setSelectedCategory}
+      />
       <div className="w-3/4 flex flex-wrap">
-        {products.map(product => (
+        {products.map((product) => (
           <ProductCard key={product.title} product={product} />
         ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default SectionWrapper(Catalase, "catalase");
