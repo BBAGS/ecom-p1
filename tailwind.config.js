dce242f53937c30c0e9cabf36c4ec8d09e2499c4
ThomasA <36617237+BBAGS@users.noreply.github.com>
2023-07-12 15:48:57 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,20 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        light: {
+          primary: '#D6FF79', // light green
+          secondary: '#B0FF92', // light green
+          accent: '#A09BE7', // light purple
+          muted: '#F0F4EF', // off-white
+        },
+        dark: {
+          primary: '#271F30', // very dark purple
+          secondary: '#3F3356', // dark purple
+          accent: '#9CBBE9', // sky blue
+          muted: '#2D283E', // very dark desaturated blue
+        },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
