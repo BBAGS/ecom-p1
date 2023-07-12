cbfa2c2392d3c300e14e076e9e48027d0bfce283
ThomasA <36617237+BBAGS@users.noreply.github.com>
2023-07-12 16:48:08 +0900

    Update CategoryTabs.tsx

--- components/navigation/CategoryTabs.tsx
+++ components/navigation/CategoryTabs.tsx
@@ -13,22 +13,23 @@ export default function CategoryTabs({
   onCategorySelect,
 }: CategoryTabsProps) {
   return (
-    <div className="flex flex-row sm:flex-col overflow-x-auto no-scrollbar">
+    <ul className="menu bg-base-200 rounded-box flex flex-row sm:flex-col overflow-x-auto no-scrollbar">
       {categories.map((category) => (
-        <motion.button
-          key={null}
-          className={`text-center py-2 px-4 rounded-lg ${
-            category === selectedCategory
-              ? "bg-light-primary dark:bg-dark-primary text-white"
-              : "bg-light-muted dark:bg-dark-muted text-black dark:text-white"
-          }`}
-          onClick={() => onCategorySelect(category)}
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.95 }}
-        >
-          {category}
-        </motion.button>
+        <li key={category}>
+          <motion.a
+            className={`${
+              category === selectedCategory
+                ? "bg-light-primary dark:bg-dark-primary text-white"
+                : "text-black dark:text-white"
+            }`}
+            onClick={() => onCategorySelect(category)}
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            {category}
+          </motion.a>
+        </li>
       ))}
-    </div>
+    </ul>
   );
 }
