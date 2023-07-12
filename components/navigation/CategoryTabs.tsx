056092f56dbed5c44a2be5504776a779783bda4a
ThomasA <36617237+BBAGS@users.noreply.github.com>
2023-07-12 17:10:18 +0900

    Update CategoryTabs.tsx

--- components/navigation/CategoryTabs.tsx
+++ components/navigation/CategoryTabs.tsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
 
 type CategoryTabsProps = {
   categories: string[];
@@ -12,17 +11,37 @@ export default function CategoryTabs({
   selectedCategory,
   onCategorySelect,
 }: CategoryTabsProps) {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  // Function to check screen size and update state accordingly
+  const handleResize = () => {
+    setIsSmallScreen(window.innerWidth < 640);
+  };
+
+  // Add event listener on component mount to track screen size changes
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className="overflow-x-auto no-scrollbar">
-      <ul className="menu bg-base-200 rounded-box flex flex-row sm:flex-col">
+    <div className="w-full max-w-[25%] overflow-x-auto no-scrollbar">
+      <ul
+        className={`menu ${
+          isSmallScreen ? "" : "menu-horizontal"
+        } bg-base-200 rounded-box flex flex-row sm:flex-col`}
+      >
         {categories.map((category) => (
-          <li key={category}>
+          <li key={category} className="mb-2 sm:mb-0">
             <a
               className={`${
                 category === selectedCategory
                   ? "bg-light-primary dark:bg-dark-primary text-white"
                   : "text-black dark:text-white"
-              }`}
+              } hover:scale-105 transition-transform block py-2 px-4 rounded-lg`}
               onClick={() => onCategorySelect(category)}
             >
               {category}
