7d8d35247ecfcdf9f989fe4c08bc19f77080a0b8
ThomasA <36617237+BBAGS@users.noreply.github.com>
2023-07-12 16:26:06 +0900

    Update Catalase.tsx

--- components/sections/Catalase.tsx
+++ components/sections/Catalase.tsx
@@ -43,7 +43,7 @@ const Catalase = () => {
         selectedCategory={selectedCategory}
         onCategorySelect={setSelectedCategory}
       />
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 p-4">
+      <div className="w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 p-4">
         {products.map((product) => (
           <ProductCard key={product.title} product={product} />
         ))}
