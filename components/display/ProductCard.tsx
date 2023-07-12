97005797999cf3ec025e61a5e0d8b375bf83ce4a
ThomasA <36617237+BBAGS@users.noreply.github.com>
2023-07-12 16:09:55 +0900

    Update ProductCard.tsx

--- components/display/ProductCard.tsx
+++ components/display/ProductCard.tsx
@@ -14,7 +14,7 @@ type ProductCardProps = {
 
 export default function ProductCard({ product }: ProductCardProps) {
   return (
-    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
+    <div className="w-full sm:w-1/2 p-2">
       <motion.div
         className="rounded-lg overflow-hidden shadow-lg bg-light-muted dark:bg-dark-muted"
         whileHover={{ scale: 1.05 }}
