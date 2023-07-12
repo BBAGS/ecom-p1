a6a1e3f824090214799b1347a9c8f14a3f5ec407
ThomasA <36617237+BBAGS@users.noreply.github.com>
2023-07-12 16:05:53 +0900

    Update ProductCard.tsx

--- components/display/ProductCard.tsx
+++ components/display/ProductCard.tsx
@@ -14,9 +14,9 @@ type ProductCardProps = {
 
 export default function ProductCard({ product }: ProductCardProps) {
   return (
-    <div className="w-1/4 p-4">
+    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
       <motion.div
-        className="rounded-lg overflow-hidden shadow-lg dark:bg-slate-800"
+        className="rounded-lg overflow-hidden shadow-lg bg-light-muted dark:bg-dark-muted"
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95 }}
       >
@@ -28,12 +28,12 @@ export default function ProductCard({ product }: ProductCardProps) {
             style={{ objectFit: "cover" }}
           />
         </div>
-        <div className="px-6 py-4 dark:text-[#a6adba]">
+        <div className="px-6 py-4 dark:text-white">
           <div className="font-bold text-xl mb-2">{product.title}</div>
           <p className="text-gray-700 dark:text-gray-300 text-base">{product.description}</p>
         </div>
         <div className="px-6 pt-4 pb-2">
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 dark:bg-slate-700 dark:text-[#a6adba]">
+          <span className="inline-block bg-light-primary dark:bg-dark-primary rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 dark:text-white">
             ${product.price}
           </span>
         </div>
