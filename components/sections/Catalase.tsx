b079a583ea2be2b67e058d4de1b51de66f94cbca
Thomas Aditya Hanafi (トーマス アディティア ハナフィ) <y03024@yagami.co.jp>
2023-07-14 17:03:08 +0900

    from office 3

--- components/sections/Catalase.tsx
+++ components/sections/Catalase.tsx
@@ -41,14 +41,14 @@ const Catalase = () => {
       title: "Product 1",
       description: "Description 1",
       image:
-        "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80",
+        "https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80",
       price: "10.00",
     },
     {
       title: "Product 2",
       description: "Description 2",
       image:
-        "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80",
+        "https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80",
       price: "20.00",
     },
     // More products...
