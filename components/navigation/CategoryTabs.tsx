9528bbb3a03d75505e8ade2aae66622719bcfca3
Thomas Aditya Hanafi (トーマス アディティア ハナフィ) <y03024@yagami.co.jp>
2023-07-14 16:55:37 +0900

    from office 2

--- components/navigation/CategoryTabs.tsx
+++ components/navigation/CategoryTabs.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
+import { Category } from "../sections/Catalase";
 
 type CategoryTabsProps = {
-  categories: string[];
+  categories: Category[];
   selectedCategory: string;
   onCategorySelect: (category: string) => void;
 };
@@ -35,14 +36,16 @@ export default function CategoryTabs({
       >
         {categories.map((category, index) => (
           <li
-            key={category}
+            key={category.name}
             className={`mb-0 sm:mb-2 ${
               index !== categories.length - 1 ? "mr-2 sm:mr-0" : ""
             }`}
           >
             <a
-              className={`${category === selectedCategory ? "active" : ""}`}
-              onClick={() => onCategorySelect(category)}
+              className={`${
+                category.name === selectedCategory ? "active" : ""
+              }`}
+              onClick={() => onCategorySelect(category.name)}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -55,10 +58,10 @@ export default function CategoryTabs({
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                  d={category.path}
                 />
               </svg>
-              {category}
+              {category.name}
             </a>
           </li>
         ))}
--- components/sections/Catalase.tsx
+++ components/sections/Catalase.tsx
@@ -5,6 +5,11 @@ import CategoryTabs from "../navigation/CategoryTabs";
 import ProductCard from "../display/ProductCard";
 import { SectionWrapper } from "@/hoc";
 
+export type Category = {
+  name: string;
+  path: string;
+};
+
 type Product = {
   title: string;
   description: string;
@@ -15,6 +20,21 @@ type Product = {
 // function Catalase() {
 const Catalase = () => {
   const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const categories: Category[] = [
+    {
+      name: "All",
+      path: "M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125z",
+    },
+    {
+      name: "TV",
+      path: "M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25",
+    },
+    {
+      name: "Category 3",
+      path: "M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z",
+    },
+  ]; // Replace with your actual categories
 
   const products: Product[] = [
     {
@@ -33,8 +53,6 @@ const Catalase = () => {
     },
     // More products...
   ];
-
-  const categories = ["All", "Category 1", "Category 2", "Category 3"]; // Replace with your actual categories
 
   return (
     <section className="grid grid-cols-4 gap-4 max-w-7xl mx-auto p-4">
